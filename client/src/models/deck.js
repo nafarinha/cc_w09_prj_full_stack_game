1a0cc61a0c60487d6250560becc9521b141af18c
ramosjm <jjramos_2004@hotmail.com>
2019-03-11 15:54:58 +0000

    pseudo code to create cards in Deck model.

--- client/src/models/deck.js
+++ client/src/models/deck.js
@@ -4,8 +4,31 @@ const Card = require('./card.js');
 
 
 const Deck = function (highestNumber = null, numberOfCards = null ) {
-
+  this.cards = [];
   this.highestNumber = highestNumber;
   this.numberOfCards = numberOfCards;
 
 };
+
+//resposible for getting the highestNumber OR number of cards.
+
+// checkChangedAttribute
+  // if this.highestNumber is not null then run fn getNumberOfCards
+  // else run fn getHighestNumber
+//
+
+//getNumberOfCards
+  // Math.log2(this.highestNumber)
+  //  Math.ceil(result of log2)
+  // assing result of ceil to this.numberOfCards.
+
+//getHighestNumber
+ // 2 to the power of this.numberOfCards
+ // floor the result above
+ // asign resul of floor to this.highestNumber
+
+//generateCards
+//using this.numberOfCards
+// loop up to this.numberOfCards
+// for each iteration create a Card object and push it into this.cards.
+// ***this will be called from App.js
