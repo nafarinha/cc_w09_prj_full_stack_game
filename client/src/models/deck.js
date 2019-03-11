3229dc4a6e50ae1f1abef67c7610aad0bbc3a0b9
ramosjm <jjramos_2004@hotmail.com>
2019-03-11 16:33:13 +0000

    added more pseudo code, logic to guess number.

--- client/src/models/deck.js
+++ client/src/models/deck.js
@@ -3,14 +3,23 @@ const PubSub = require('../helpers/pub_sub.js');
 const Card = require('./card.js');
 
 
-const Deck = function (highestNumber = null, numberOfCards = null ) {
+const Deck = function () {
   this.cards = [];
-  this.highestNumber = highestNumber;
-  this.numberOfCards = numberOfCards;
+  this.highestNumber = null;
+  this.numberOfCards = null;
 
 };
 
 //resposible for getting the highestNumber OR number of cards.
+
+//updateHighestNumber(number) **number will come from app.js
+// assign number to this.highestNumber
+//checkChangedAttribute()
+
+
+//updateNumberOfCards(number) **number will come from app.js
+// assign number to this.numberOfCards
+//checkChangedAttribute()
 
 // checkChangedAttribute
   // if this.highestNumber is not null then run fn getNumberOfCards
@@ -31,4 +40,15 @@ const Deck = function (highestNumber = null, numberOfCards = null ) {
 //using this.numberOfCards
 // loop up to this.numberOfCards
 // for each iteration create a Card object and push it into this.cards.
+//randomiseCards
 // ***this will be called from App.js
+
+
+//randomiseCards
+// use this.cards and randomise the position in the cards array.
+
+//getGuessedNumber
+// use this.cards - loop through each card
+// if card.containsGuessedNumber is true sum the first element of the array of each card.
+// publish total sum.
+//*** this will come from app.js
