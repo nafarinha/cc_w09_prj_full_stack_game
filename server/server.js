540cc689f85facf059073bfdb55a36de85f03e81
ramosjm <jjramos_2004@hotmail.com>
2019-03-11 11:25:33 +0000

    edited server.js with comments.

--- server/server.js
+++ server/server.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const parser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
 
-app.use(parser.json());
+//review server.js for front end use.
 
-MongoClient.connect('mongodb://localhost:27017')
-.then((client) => {
-    const db = client.db('#####');
-    const listsCollection = db.collection('#####');
-    const listsRouter = createRouter(listsCollection);
-    app.use('######', listsRouter);
-  })
-  .catch(console.err);
-
-  app.listen(3000, function () {
-    console.log(`Listening on port ${this.address().port}`);
-  });
+app.listen(3000, function () {
+  console.log(`Listening on port ${this.address().port}`);
+});
