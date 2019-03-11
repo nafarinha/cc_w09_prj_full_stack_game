e0aa81d45f88e0e714c91d31e1fb16fc15336a97
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-03-11 18:18:44 +0000

    changed card spec to deck_spec

--- client/src/specs/deck_spec.js
+++ client/src/specs/deck_spec.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
-const Cards = require('../models/cards.js');
+const Deck = require('../models/deck.js');
 
-describe('Cards', function() {
+describe('Deck', function() {
 
-  let cards;
+  let deck;
 
   beforeEach(function() {
-    cards = [
+    deck = [
   {
   card:[2, 3, 6, 7, 10, 11, 14, 15, 18, 19, 22, 23, 26, 27, 30, 31, 34, 35, 38, 39, 42, 43, 46, 47, 50, 51, 54, 55, 58, 59, 62, 63, 66, 67, 70, 71, 74, 75, 78, 79, 82, 83, 86, 87, 90, 91, 94, 95, 98, 99, 102, 103, 106, 107, 110, 111, 114, 115, 118, 119, 122, 123, 126, 127],
   containsNumber: false,
@@ -62,7 +62,7 @@ describe('Cards', function() {
 // });
 
 xit('should generate random display order of cards', function() {
-  
+
 });
 
 xit('should calculate number of cards based on highest number', function() {
