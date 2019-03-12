d9ab596af01075d2d27441fadcb17342550c7663
Elisa Bjorg Sveinsdottir <elisabjorg22@gmail.com>
2019-03-12 17:43:06 +0000

    updated card and board view

--- client/src/views/board_view.js
+++ client/src/views/board_view.js
@@ -1,20 +1,41 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const BoardView = function (card) {
-  this.card = card;
+const BoardView = function () {
+  this. = ;
 };
 
-// Gets a single-card-ready from the Deck model
-//
-// Channel: Cards:single-card-ready
-// Subscribes
-// (Deck Model publishes)
 
-//InfoView.prototype.bindEvents = function() {
-  //PubSub.subscribe('Cards:single-card-ready', (evt) => {
+
+
+// Takes the answer from player 'yes' or 'no' and publishes to the model
+//Needs two BUTTONS
+
+// Publishes
+// Channel CardView:user-answer
+// (Deck model subscribes)
+
+
+//CardView.prototype.bindEvents = function() {
+  //PubSub.publish('CardView:user-answer', (evt) => {
 
 //})
 //}
 
+// /BUTTON
+// / BoardView.prototype.bindEvents = function() {
+// //   this.answer.addEventListener('submit', (evt) => {
+// //  //choose YES(this.answerYes(evt))
+// //   })
+// // }
+
+// Function for what happens when player presses Yes. Needs to sum those values of Cards
+//together to get the final sum result in the end.
+//And we want to go straight to the next page when we answer.
+
+//CardView.prototype.answerYes = function() {
+
+}
+
+//if player presses No then nothing happens
 
 module.exports = BoardView;
--- client/src/views/card_view.js
+++ client/src/views/card_view.js
@@ -1,40 +1,24 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const CardView = function (answer) {
-  this.answer = answer;
+const CardView = function (container, prettyCardObject) {
+  this.= ;
 };
 
 
-// Takes the answer from player 'yes' or 'no' and publishes to the model
-//Needs two BUTTONS
+//each card takes a number range (min/max)
+// dont have to do a pubsub here. Do it in app.js. use deck model,
+//access one card and pass them into boardview and pass a singlecard
+//into cardview.
 
-// Publishes
-// Channel CardView:user-answer
-// (Deck model subscribes)
+//render card
+//CardView.prototype.render = function() {
 
+//create card here
+//createElement p for the numbers
+//the cardContainer has to have an id of card container.
+//you can do this in html #
+// }
 
-//CardView.prototype.bindEvents = function() {
-  //PubSub.publish('CardView:user-answer', (evt) => {
-
-//})
-//}
-
-// /BUTTON
-// / FormView.prototype.bindEvents = function() {
-// //   this.answer.addEventListener('submit', (evt) => {
-// //  //choose YES(this.answerYes(evt))
-// //   })
-// // }
-
-// Function for what happens when player presses Yes. Needs to sum those values of Cards
-//together to get the final sum result in the end.
-//And we want to go straight to the next page when we answer. 
-
-//CardView.prototype.answerYes = function() {
-
-}
-
-//if player presses No then nothing happens
 
 
 
