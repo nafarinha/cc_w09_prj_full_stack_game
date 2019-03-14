160e06f0f5a752607933d792ddb73ec33981c525
ramosjm <jjramos_2004@hotmail.com>
2019-03-14 00:36:59 +0000

    submit button changed to a regular button.

--- client/src/views/form_view.js
+++ client/src/views/form_view.js
@@ -16,6 +16,13 @@ FormView.prototype.setupEventListeners = function(){
       const playButton = document.querySelector('#play-game');
       playButton.classList.replace('hidden','show');
       PubSub.publish('FormView:number-inputted', evt.target.value);
+
+      playButton.addEventListener('click',function(){
+        const inputBoxValue = document.querySelector('#cards-number-container').value;
+        PubSub.publish('FormView:sumbit',inputBoxValue);
+        const formElement = document.querySelector('#games-form');
+        formElement.classList.replace('show','hidden');
+      });
     });
   });
 
@@ -25,85 +32,8 @@ FormView.prototype.setupEventListeners = function(){
   //   inputBox1.classList.replace('hidden','show');
   //   console.log(inputBox1);
   // });
-  const playButton = document.querySelector('#games-form');
-  playButton.addEventListener('submit',(evt)=>{
-    evt.preventDefault();
-    PubSub.publish('FormView:sumbit',evt.target.cardsNumber.value);
-    playButton.innerHTML ='';
-  });
+
 };
-
-
-
-// The start site with Title - Information - option to play by number of cards
-// or the highest number.
-// Press a BUTTON to choose to play by cards or press a BUTTON to play by
-// the highest number.
-// Input your number.
-// Then get the INFOVIEW sent with information about highest number or number of cards
-// Then press PLAY BUTTON
-
-//
-// Publishes
-// Channel: FormView:number-submitted
-// (Deck model subscribes)
-
-//We need another PubSub for the PLAY BUTTOn
-
-//BUTTON
-// FormView.prototype.bindEvents = function() {
-//   this.form.addEventListener('submit', (evt) => {
-//  //choose to use to play by cards(this.numberOfCards(evt))
-//   })
-// }
-
-//BUTTON
-// / FormView.prototype.bindEvents = function() {
-// //   this.form.addEventListener('submit', (evt) => {
-// //  //choose to use to play by highest number(this.highestNumber(evt))
-//
-// //   })
-// // }
-
-// /BUTTON
-// / FormView.prototype.bindEvents = function() {
-// //   this.form.addEventListener('submit', (evt) => {
-// //  //choose to use to start game(this.handleSubmit(evt))
-// //   })
-// // }
-
-//(FormView.prototype.render = function (game) {
-  // const gameForm = document.createElement('div');
-  // gameForm.id = 'game';)
-// }
-
-//if player chooses to play by numberOfCards
-//FormView.prototype.numberOfCards = function (evt) {
-  // evt.preventDefault();
-  //const playCards =
-  //player inserts number
- //gets information from INFOVIEW
-
-// }
-
-//if player chooses to play by highestNumber
-//FormView.prototype.highestNumber = function (evt) {
-  // evt.preventDefault();
-  //const playHighestNumber =
-  //player inserts number
- //gets information from INFOVIEW
-
-// }
-
-//handle the Play button - then we go to the boardView(and cardView)
-// FormView.prototype.handleSubmit = function (evt) {
-//   evt.preventDefault();
-//   const startGame = this.newGame(evt.target);
-//
-// };
-
-//   return startGame;
-// };
 
 
 
